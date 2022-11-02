bc7e8d0d3b5b302e1a329d03b8ddf8a765e9f28a
Shmarin Mikhail <shmarinms@gmail.com>
2022-11-02 05:21:24 +0300

    adding dependencies for TODO

---
+++ src/main/java/ru/penza/encyclopedia/EncyclopediaApp.java
@@ -0,0 +1,11 @@
+package ru.penza.encyclopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncyclopediaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EncyclopediaApp.class, args);
+    }
+}
